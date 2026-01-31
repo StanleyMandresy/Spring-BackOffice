fa524d67c0c4dab00fcadec4c5f38a2872783b2d
StanleyMandresy <mandresystanley@gmail.com>
2026-02-01 02:10:16 +0300

    fix framework

--- deploy-back.bat
+++ deploy-back.bat
@@ -5,10 +5,25 @@ REM ======================================
 
 SET "TOMCAT_HOME=C:\apache-tomcat-10.1.48-windows-x64\apache-tomcat-10.1.48"
 SET "PROJECT_HOME=D:\dev\Naina\Devops\BackOffice"
+SET "APP_NAME=sprint0"
 
-REM Copier le WAR vers Tomcat et renommer
-copy /Y "%PROJECT_HOME%\target\back-office-0.0.1-SNAPSHOT.war" "%TOMCAT_HOME%\webapps\sprint0.war"
+echo Arrêt de Tomcat...
+call "%TOMCAT_HOME%\bin\shutdown.bat"
+timeout /t 10 /nobreak >nul
 
+echo Suppression de l'existant...
+if exist "%TOMCAT_HOME%\webapps\%APP_NAME%.war" (
+    del /F /Q "%TOMCAT_HOME%\webapps\%APP_NAME%.war"
+)
+if exist "%TOMCAT_HOME%\webapps\%APP_NAME%" (
+    rmdir /S /Q "%TOMCAT_HOME%\webapps\%APP_NAME%"
+)
+
+echo Copie du nouveau WAR...
+copy /Y "%PROJECT_HOME%\target\%APP_NAME%.war" "%TOMCAT_HOME%\webapps\%APP_NAME%.war"
+
+echo Démarrage de Tomcat...
+start "" "%TOMCAT_HOME%\bin\startup.bat"
 
 echo Déploiement terminé !
 pause
